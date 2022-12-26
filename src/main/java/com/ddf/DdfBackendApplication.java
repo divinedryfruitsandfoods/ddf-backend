c5471e5bc5078e5d3554c4a24b68a9d1311b16fc
divinedryfruitsandfoods <rushikeshkarkhanis@Rushikeshs-MacBook-Pro.local>
2022-12-26 16:31:22 +0530

    First Commit

---
+++ src/main/java/com/ddf/DdfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ddf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddf/DdfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
